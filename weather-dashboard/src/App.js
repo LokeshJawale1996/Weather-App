3d05f2427e586118cb513bb213b6b55f864bbfe3
lokeshjawale1996 <jawalelokesh2411@gmail.com>
2023-02-05 01:48:02 +0530

    Project created with tailwind css setup for weather dashboard

---
+++ weather-dashboard/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-blue-400 font-bold text-2xl'>Welcome To Weather DashBoard</h1>
+    </div>
+  );
+}
+
+export default App;
