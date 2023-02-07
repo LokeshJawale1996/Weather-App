3750a0e3642f073a4c9426845fa8e67a6a316545
lokeshjawale1996 <jawalelokesh2411@gmail.com>
2023-02-07 18:14:05 +0530

    added loader when we search first time or click on Show Weather

--- weather-dashboard/src/components/Weather.js
+++ weather-dashboard/src/components/Weather.js
@@ -11,6 +11,9 @@ import Data from "../config.json";
 function Weather() {
   const [select, setSelect] = useState("");
   const [data, setData] = useState(null);
+  const [completeData, setCompleteData] = useState([]);
+  const [showLoader, setShowLoader] = useState(false);
+  const [showHome, setShowHome] = useState(true);
 
   //as per we change value on dropdown it get setted in select state
   const handleChange = (e) => {
@@ -23,6 +26,8 @@ function Weather() {
   };
   //after clicking on add it executes
   const handleClick = () => {
+    setShowLoader(true);
+
     const fetchApi = async () => {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${select}&units=metric&appid=3f256663324712b90cf713a480e25f6d
           `;
@@ -31,6 +36,12 @@ function Weather() {
       //we are setting response in state
       // console.log(resJson.name)
       setData(resJson);
+      if (resJson) {
+        setShowLoader(false);
+      }
+      setCompleteData(() => [...completeData, data]);
+      console.log("dddddd", completeData);
+
       //fixed the issue when we pass city which is inValid
       if (resJson.name === select) {
         console.log("yessssss");
@@ -39,6 +50,7 @@ function Weather() {
         window.location.reload(false);
       }
     };
+
     setTimeout(() => {
       fetchApi();
     }, 200);
@@ -107,7 +119,7 @@ function Weather() {
                   type="submit"
                   onClick={handleClick}
                 >
-                  add
+                  Show Weather
                 </button>
               </div>
               <div className="flex">
@@ -142,14 +154,40 @@ function Weather() {
           </div>
         </div>
       </div>
-
+      {showLoader && !data && (
+        <div role="status" className="bg-white text-center flex justify-center">
+          <svg
+            aria-hidden="true"
+            class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span class="sr-only">Loading...</span>
+        </div>
+      )}
       {/* Data/Cards */}
 
-      {!data ? (
+      {!data && showHome ? (
         <p className="flex text-center h-full justify-center justify-items-center py-64 bg-slate-200 text-3xl">
           Please Select City Name OR Enter City To See It's Weather Condition!
         </p>
       ) : (
+        // completeData && completeData.map((data,index) => (
+        //   <div key={index}>
+        //     {cityData}.name}
+        //   </div>
+        //     )
+        // )
         <div className="mx-auto px-12 py-3 my-3  max-w-xl max-h-full">
           <div className="max-h-full  p-4 border-4 border-gray-400 rounded-3xl shadow sm:p-6 dark:bg-gray-800 dark:border-gray-900  bg-gradient-to-r from-cyan-100 to-blue-300">
             <ul>
@@ -159,7 +197,7 @@ function Weather() {
               >
                 <div className="flex gap-x-4">
                   <p>{data.name},</p>
-                  <p>({data.sys.country})</p>
+                  <p>{data.sys.country}</p>
                 </div>
 
                 <div className="text-right">
@@ -247,17 +285,8 @@ function Weather() {
                   </p>
                 </div>
               </li>
-              {/* <li>
-                
-              </li> */}
             </ul>
           </div>
-          {/* <div className="bg-white max-h-screen w-full h-full max-w-sm p-4 border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            Two
-          </div>
-          <div className="bg-white max-h-screen w-full h-full max-w-sm p-4 border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            Three
-          </div> */}
         </div>
       )}
     </div>
